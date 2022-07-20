18f6d6b62fa895df32b94c30588da35a5782fc64
jaurif <jauricflowers@gmail.com>
2022-07-20 15:02:10 -0700

    setting up kanye api

---
+++ db.js
@@ -0,0 +1,17 @@
+export const destinations = {
+    189723: {
+        destination: "Eiffel Tower",
+        location: "Paris",
+    },
+
+    987654: {
+        destination: "Big Ben",
+        location:"London",
+    },
+};
+
+
+
+// module.exports = {
+//     destinations,
+// }
---
+++ index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import express from "express";
+import cors from "cors";
+
+const URL = "https://api.kanye.rest/"
+const app = express();
+const PORT = 3000;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+app.use(cors())
+// app.use()
+
+app.get('/kanye_quotes',
+    async (req, res) => {
+        try {
+            const response = await axios.get(URL)
+            res.send(response.data);
+        } catch (e) {
+            console.log(e);
+        }
+    });
+
+app.get('/', (req, res) => {
+    res.send("Hello Jay");
+})
